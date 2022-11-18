0530be61e4a1c141cd64648050eee062a73908ce
Yongkan Huang <yongkan@connectly.ai>
2022-11-18 14:40:16 +0800

    update

--- ratelimit.go
+++ ratelimit.go
@@ -9,54 +9,50 @@ import (
 const script = `
 -- key
 local key = KEYS[1]
--- max number of tokens
-local max_permits = 100000000
 -- duration, in second
-local duration = tonumber(KEYS[2])
--- number of produced tokens per duration
-local permits_per_duration = tonumber(KEYS[3])
+local duration = tonumber(ARGV[1])
+-- max number of tokens per duration
+local limit = tonumber(ARGV[2])
 -- required tokens
-local required_permits = tonumber(ARGV[1])
+local n = tonumber(ARGV[3])
+
+local micro_seconds_in_second = 1000000
 
 -- current time
 local time = redis.call('time')
-local now_micros = tonumber(time[1]) * 1000000 + tonumber(time[2])
+local now_micros = tonumber(time[1]) * micro_seconds_in_second + tonumber(time[2])
 
 local next_free_ticket_micros = tonumber(redis.call('hget', key, 'next_free_ticket_micros') or now_micros)
 
 -- current stored tokens
-local stored_permits = tonumber(redis.call('hget', key, 'stored_permits') or 0)
+local stored_permits = tonumber(redis.call('hget', key, 'stored_permits') or limit)
 -- fill interval per token
-local stable_interval_micros = 1000000 * duration / permits_per_duration
+local stable_interval_micros = micro_seconds_in_second * duration / limit
 
 -- produce token
 if (now_micros > next_free_ticket_micros) then
     local new_permits = (now_micros - next_free_ticket_micros) / stable_interval_micros
-    stored_permits = math.min(max_permits, stored_permits + new_permits)
+    stored_permits = math.min(limit, stored_permits + new_permits)
     next_free_ticket_micros = now_micros
 end
 
--- consume token
-local moment_available = next_free_ticket_micros
-local stored_permits_to_spend = math.min(required_permits, stored_permits)
-local fresh_permits = required_permits - stored_permits_to_spend;
-local wait_micros = fresh_permits * stable_interval_micros
-
-redis.replicate_commands()
-redis.call('expire', key, duration*2)
-
-if moment_available == now_micros then
-    redis.call('hset', key, 'next_free_ticket_micros', next_free_ticket_micros + wait_micros)
-    redis.call('hset', key, 'stored_permits', stored_permits - stored_permits_to_spend)
+local allowed = 0
+if stored_permits >= n then 
+	allowed = 1
+	stored_permits = stored_permits -n
 end
 
--- return wait time for available token
-return moment_available - now_micros
+-- consume token
+redis.replicate_commands()
+redis.call('hset', key, 'next_free_ticket_micros', next_free_ticket_micros)
+redis.call('hset', key, 'stored_permits', stored_permits)
+redis.call('expire', key, duration)
+
+return allowed
 `
 
 var (
-	rlScript *redis.Script
-	pool     *redis.Pool
+	pool *redis.Pool
 )
 
 func dial(network, address, password string) (redis.Conn, error) {
@@ -74,8 +70,6 @@ func dial(network, address, password string) (redis.Conn, error) {
 }
 
 func Init(address, password string) {
-	rlScript = redis.NewScript(3, script)
-
 	pool = &redis.Pool{
 		MaxIdle:     200,
 		MaxActive:   1000,
@@ -92,7 +86,7 @@ func Init(address, password string) {
 }
 
 // take requires token at a limit per duration for key.
-func Take(key string, duration, limit, requires int) (int64, error) {
+func Allow(key string, duration, limit, requires int) (int64, error) {
 	return take(key, duration, limit, requires, pool)
 }
 
@@ -107,6 +101,7 @@ func take(key string, duration, limit, requires int, pool *redis.Pool) (int64, e
 		return 0, err
 	}
 
+	rlScript := redis.NewScript(1, script)
 	reply, err := rlScript.Do(c, key, duration, limit, requires)
 	if err != nil {
 		return 0, err
--- ratelimit_test.go
+++ ratelimit_test.go
@@ -1,13 +1,34 @@
-package ratelimit
+package ratelimit_test
 
 import (
+	"github.com/seaguest/log"
+	"github.com/seaguest/ratelimit"
 	"testing"
+	"time"
 )
 
+/*
 func BenchmarkFoo(b *testing.B) {
 	Init("127.0.0.1:6379", "")
 
 	for i := 0; i < b.N; i++ {
-		Take("1", 60, 100000, 1)
+		Allow("1", 60, 100)
+	}
+}
+*/
+func Test_A(b *testing.T) {
+
+	log.Error("xxx")
+	ratelimit.Init("172.17.0.5:6379", "")
+
+	for i := 0; i < 10; i++ {
+		c, err := ratelimit.Allow("1", 10, 10, 1)
+		log.Error(c, err)
+	}
+
+	for i := 0; i < 20; i++ {
+		c, err := ratelimit.Allow("1", 10, 10, 1)
+		time.Sleep(time.Millisecond * 500)
+		log.Error(c, err)
 	}
 }
